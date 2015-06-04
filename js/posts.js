9f607cd4e7c674507741ff55391adb7a189aad02
jamesstaub <james.staub@gmail.com>
2015-06-03 20:28:31 -0400

    added setter method the handlebars IFFE so the function could be repurposed for different resources

--- js/posts.js
+++ js/posts.js
@@ -2,37 +2,75 @@
 
 var apiURL = "http://localhost:3000";
 
-var displayposts = (function(){
-  var getPosts = function(){
-    $.get( apiURL+"/posts").done(function(response){
-      _renderPosts(response);
+// var displayPosts = (function(){
+//   var getPosts = function(){
+//     $.get( apiURL+"/posts").done(function(response){
+//       _renderPosts(response);
+//     });
+//   };
+//   var _renderPosts = function(displayPosts){
+//     // your code starts here
+//     var templatingFunction = Handlebars.compile($('#display-posts').html());
+//     var result = templatingFunction({
+//       posts: displayPosts
+//     });
+//     console.log(displayPosts)
+//     $('#posts-container').html(result);
+//   };
+//   return {
+//     indexPosts: getPosts
+//   };
+// })();
+
+// use the plural resource when passing in the argument
+// the handlebars script tag in the html must be named <script id="resource-container">
+// currently only works for nested resources like post: [{key: val, key, val}, {key: val, key, val}]
+var displayPosts = (function(resource){
+  console.log(resource + ' is the name before setter');
+  var setResourceName = function(resName){
+    resource = resName;
+    console.log(resource + ' is the name after setter');
+  }
+  var getResource = function(){
+    $.get( apiURL+"/"+resource).done(function(response){
+      _renderResourceEach(response);
     });
   };
-  var _renderPosts = function(displayposts){
-    // your code starts here
-    var templatingFunction = Handlebars.compile($('#display-posts').html());
+  var _renderResourceEach = function(renderedResource){
+
+    var templatingFunction = Handlebars.compile($('#display-'+resource).html());
     var result = templatingFunction({
-      posts: displayposts
+      resource: renderedResource
     });
-    $('#posts-container').html(result);
+    $('#'+resource+'-container').html(result);
   };
   return {
-    indexPosts: getPosts
+    setResourceName: setResourceName,
+    indexResource: getResource
   };
-
 })();
 
 
+
+
+
+
+
 $(document).ready(function(){
-   // invoke the above IFFE on pageload, to get all the posts and render in handlebars
-  displayposts.indexPosts();
+  // set the name of the resource we are iterating over
+  displayPosts.setResourceName("posts");
+  // invoke the above IFFE on pageload, to get all the posts and render in handlebars
+  displayPosts.indexResource();
+
+
+
 
 //submit a comment on a post
   $('body').on('click', '.submit-reply', function(){
     var thisPostID = $(this).data('post-id');
     var thisReplyVal = $('input.input-reply[data-post-id='+thisPostID+' ]').val();
 
-    console.log();
+// POST create a new reply
     $.post(apiURL+"/replies",
     {
       reply: {
